be81c266665a67463e5deb5a99c1e15e8d34d8cf
Suto Musima <sutomusima@gmail.com>
2022-03-13 22:19:56 +0000

    Use printf command in a C file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *main - main block
+ *main -Entry point
  *
- * 
- *Return: string statement 
+ * Return: 0
  */
 int main(void)
 {	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 
-}	
+}
+
