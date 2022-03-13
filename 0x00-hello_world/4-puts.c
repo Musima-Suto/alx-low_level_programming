91935475315f5675fa3c80f20b6b7381452bf483
Suto Musima <sutomusima@gmail.com>
2022-03-13 20:49:32 +0000

    Use the puts command to display output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ *main - mai block 
+ *Return:0
+ */
+
 int main(void)
 {
 	char string[70];
-	strcpy(string, "Programming is like building a multilingual puzzle");
+	strcpy(string, "\"Programming is like building a multilingual puzzle");
 	puts(string);
-	return 0;	
+	return (0);	
 }
