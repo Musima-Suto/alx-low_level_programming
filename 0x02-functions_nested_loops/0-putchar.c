6e54381a78075b94a33f8385ea7259713fbec7b6
Suto Musima <sutomusima@gmail.com>
2022-03-15 16:43:08 +0000

    Print C file contents

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  *main - print contents of a file
- *
+ *Return:0
  */
 int main(void)
 {
 	char c[] = "_putchar";
 	int n = 0;
 
-	while (n <=13)
+	while (n <= 7)
 	{
 		_putchar(c[n]);
 		n++;
