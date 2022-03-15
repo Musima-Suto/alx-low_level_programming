27cbd0547cba39479f9dab67ab7745729e9cd133
Suto Musima <sutomusima@gmail.com>
2022-03-15 18:35:11 +0000

    Print lowercase letters using putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *print_alphabet - print lowercase letters
+ *Return:0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
