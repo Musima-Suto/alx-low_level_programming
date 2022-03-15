30beaeabef24db92f9eab4df93abab3ec376423f
Suto Musima <sutomusima@gmail.com>
2022-03-15 01:01:09 +0000

    Print numbers 0 to 10 without char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - print numbers from 0 to 10 without using char
+ *Return:0
+ **/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
