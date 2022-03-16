2cd5d9d4230318844926bc4dde222026f519b990
Suto Musima <sutomusima@gmail.com>
2022-03-16 01:30:01 +0000

    Print natural numbers upto 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_to_98 - print all natural numbers from n to 98
+ *@n: the initial number
+ *Return:0
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d ", n);
+		n++;
+	}
+}
