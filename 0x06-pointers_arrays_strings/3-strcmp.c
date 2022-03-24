6aa06495150d1db2c0574093c4f12bf8673ac451
Suto Musima <sutomusima@gmail.com>
2022-03-24 21:49:42 +0000

    Function similar strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strcmp - function similar to strcmp
+ *@s1: string under comparison
+ *@s2: string under comparison
+ *Return: the difference between the number of characters
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] && s1 == s2)
+	{
+		i++
+	}
+	if (s1 != s2)
+	{
+		difference = s1 - s2
+	}
+	return (difference);
+}
