4462875c0f5a4315d34bb7d459dea855b8c42015
Musima-Suto <60604711+Musima-Suto@users.noreply.github.com>
2022-03-29 01:10:08 +0300

    Function similar to strpbk

---
+++ 0x07-pointers_arrays_strings/4-strpbk.c
@@ -0,0 +1,28 @@
+#include "mainn.h"
+
+/**
+ * _strpbrk - locates the first occurrence in a string
+ *of the string being searched for.
+ * @s: The string to be searched.
+ * @accept: The set of bytes to be searched for.
+ *
+ * Return: If a set is matched - a pointer to the matched byte.
+ *         If no set is matched - NULL.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+
+		s++;
+	}
+
+	return ('\0');
+}
