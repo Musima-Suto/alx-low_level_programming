5fb7ffcdf908d0e750273a448ed87c2df72cce3b
Musima-Suto <60604711+Musima-Suto@users.noreply.github.com>
2022-03-28 23:59:24 +0300

    Function similar to memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_memset - copies n bytes of address *src
+ *to the address dest
+ *@dest: memory area to be filled
+ *@src: memory area to be copied
+ *@n: the bytes to be copied from *src
+ *Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
