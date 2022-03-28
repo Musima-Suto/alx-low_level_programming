9606d5031028c10e729017d4e0a2d36a0be28fe9
Suto Musima <sutomusima@gmail.com>
2022-03-28 14:44:56 +0000

    Function similar to memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_memset - fills the first n bytes of address *s
+ *with the constant byte b
+ *@s: memory area to be filled
+ *@b: constant byte
+ *@n: the bytes to be filled in *s
+ *Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		[i] = c;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+C tutorials on pointers, strings and arrays
