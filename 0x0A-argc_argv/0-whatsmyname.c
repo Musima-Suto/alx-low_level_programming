982d3ce7f897423e5d00f14cb78a5ca985971f11
Musima-Suto <60604711+Musima-Suto@users.noreply.github.com>
2022-04-01 14:50:57 +0300

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - A program that prints it's name, followed by a new line.
+ * @argc: The argument count
+ * @argv: the argument vector
+ * Return: Always 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
