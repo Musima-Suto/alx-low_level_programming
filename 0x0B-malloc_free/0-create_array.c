9cbb0904a09067952ae6f62cc122226f80383eb8
Suto Musima <sutomusima@gmail.com>
2022-04-05 14:12:28 +0000

    Create an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *create_array - create an array of char
+ *and intialize it with a specific char
+ *@size: size of the array
+ *@c: character intialized to the array
+ *Return: NULL if not successful, else 
+ *        Pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	ptr = malloc(sizeof(c) * size);
+
+	if (size = 0 || ptr = 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+ C tutorials on dynamic memory allocation.
