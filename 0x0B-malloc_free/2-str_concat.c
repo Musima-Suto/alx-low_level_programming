dfd08f5e4c84acf35ce5213416242800d4c2fa85
Musima-Suto <60604711+Musima-Suto@users.noreply.github.com>
2022-04-06 23:42:44 +0300

    Concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,26 +9,46 @@
  *         Pointer if successful
  */
 
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	char *pdup;
-	int i;
-	int len = 0;
+	char *new_str, *starts1, *starts2;
+	int i = 0, lens1 = 0, lens2 = 0;
 
-	if (str == NULL)
+	starts1 = s1;
+	starts2 = s2;
+	if (s1 == NULL)
+		s1 = "";
+	while (*s1)
+	{
+		lens1++;
+		s1++;
+	}
+	s1 = starts1;
+	if (s2 == NULL)
+		s2 = "";
+	while (*s2)
+	{
+		lens2++;
+		s2++;
+	}
+	s2 = starts2;
+	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	starts1 = new_str;
+	if (new_str == NULL)
 		return (NULL);
-
-	for (i = 0; str[i]; i++)
-		len++;
-
-	pdup = malloc(sizeof(char) * (len + 1));
-
-	if (pdup == NULL)
-		return (NULL);
-
-	for (i = 0; str[i]; i++)
-		pdup[i] = str[i];
-	pdup[len] = '\0';
-
-	return (pdup);
+	for (; i < (lens1 + lens2); i++)
+	{
+		if (i < lens1)
+		{
+			new_str[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			new_str[i] = *s2;
+			s2++;
+		}
+	}
+	new_str[i] = '\0';
+	return (starts1);
 }
