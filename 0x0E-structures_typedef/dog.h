807faa2e03f65e24cf39cd77acebbd84e943ddbb
Musima-Suto <60604711+Musima-Suto@users.noreply.github.com>
2022-04-13 00:19:41 +0300

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - store basic dog info
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: name of the owner
+ *Return:nothing
+ */
+typedef struct dog
+{
+        char *name[50];
+        float age;
+        char *owner;
+} dog_info;
+#endif
