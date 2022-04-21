15bb0455f2e79e587c39fe63c58d399631a401b5
Musima-Suto <60604711+Musima-Suto@users.noreply.github.com>
2022-04-22 00:30:04 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,33 +1,40 @@
 #include "lists.h"
+#include <string.h>
 
 /**
-  * *add_node - Adds a new node at the beggining of a new list
-  * @head: Address of head
-  * @str: Data to be added in new node
-  *
-  * Return: Address of new element
-  */
-
+ * add_node - Adds a new node at the beginning
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *ptr = malloc(sizeof(list_t));
-	int len = 0;
+	char *dup;
+	int len;
+	list_t *new;
 
-	if (ptr == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
 
-	while (str[len])
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
 		len++;
 
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
 
+	*head = new;
 
-	ptr->str = strdup(str);
-	ptr->len = len;
-	ptr->next = NULL;
-
-	ptr->next = *head;
-	*head = ptr;
-
-
-	return (*head);
+	return (new);
 }
